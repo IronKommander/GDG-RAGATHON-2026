3cc4aeaa71828986d9adac32d7b4ba89f14c6251
Aryan-Kumar117 <aryan.kumar9791@gmail.com>
2026-04-07 14:29:17 +0530

    feat: implemented nodes and edges for a corrective RAG

--- Statement-1-Insurance-Decoder/src/rag.py
+++ Statement-1-Insurance-Decoder/src/rag.py
@@ -1,8 +1,16 @@
+from langchain_core.messages import HumanMessage
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from langchain.tools import tool
 from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
+from typing import Literal
+from langchain_cohere import ChatCohere
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langgraph.prebuilt import ToolNode, tools_condition
+load_dotenv()
 
 bge_embeddings = HuggingFaceEmbeddings(
     model_name="BAAI/bge-base-en-v1.5", #Using the base BGE embedding model
@@ -12,13 +20,17 @@ bge_embeddings = HuggingFaceEmbeddings(
 )
 
 COLLECTION_NAME = "ST1_docs"
+MODEL="llama-3.3-70b-versatile"
 vectorstore = QdrantVectorStore.from_existing_collection(
     collection_name=COLLECTION_NAME,
     embedding=bge_embeddings
 )
 retriever = vectorstore.as_retriever()
 
-@tool
+class RetrieveInput(BaseModel):
+    query: str = Field(description="The specific search query or keywords to look up in the policy document.")
+
+@tool(args_schema=RetrieveInput)
 def retrieve_blog_posts(query: str) -> str:
     """Search and return info about TITAN SECURE's Universal Health & Wellness Policy."""
     docs = retriever.invoke(query)
@@ -26,8 +38,122 @@ def retrieve_blog_posts(query: str) -> str:
 
 tools = [retrieve_blog_posts]
 
-llm = init_chat_model(
-    "cohere/command-r",
-    model_provider="cohere",
-    temperature=0.7
+llm = ChatGroq(model=MODEL, temperature=0)
+llm_with_tools = llm.bind_tools(tools)
+
+def generate_query_or_respond(state: MessagesState):
+    """Call the model to generate a response based on the current state. Given
+    the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
+    """
+    response = (
+        llm_with_tools.invoke(state["messages"])
+    )
+    return {"messages": [response]}
+
+GRADE_PROMPT = (
+    "You are a grader assessing relevance of a retrieved document to a user question. \n "
+    "Here is the retrieved document: \n\n {context} \n\n"
+    "Here is the user question: {question} \n"
+    "If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n"
+    "Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."
 )
+
+class GradeDocuments(BaseModel):
+    """Grade documents using a binary score for relevance check."""
+
+    binary_score: str = Field(
+        description="Relevance score: 'yes' if relevant, or 'no' if not relevant"
+    )
+
+grader_model = ChatGroq(model=MODEL, temperature=0)
+
+def grade_documents(state: MessagesState) -> Literal["generate_answer", "rewrite_question"]:
+    """Determine whether the retrieved documents are relevant to the question."""
+    question = [msg.content for msg in state["messages"] if isinstance(msg, HumanMessage)][-1]
+    context = state["messages"][-1].content
+    prompt = GRADE_PROMPT.format(question=question, context=context)
+    response = (
+        grader_model
+        .with_structured_output(GradeDocuments).invoke(
+            [{"role": "user", "content": prompt}]
+        )
+    )
+    score = response.binary_score
+    if score == "yes":
+        return "generate_answer"
+    else:
+        return "rewrite_question"
+
+REWRITE_PROMPT = (
+    "Look at the input and try to reason about the underlying semantic intent / meaning.\n"
+    "Here is the initial question:"
+    "\n ------- \n"
+    "{question}"
+    "\n ------- \n"
+    "Formulate an improved question:"
+)
+
+def rewrite_question(state: MessagesState):
+    """Rewrite the orignal user question"""
+    message = state["messages"]
+    question = [msg.content for msg in state["messages"] if isinstance(msg, HumanMessage)][-1]
+    prompt = REWRITE_PROMPT.format(question=question)
+    response = grader_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [HumanMessage(content=response.content)]}
+
+GENERATE_PROMPT = (
+    "You are a RAG model that simplifies complex 'Terms & Conditions' or Policy documents."
+    "You must use a comprehensive document (Titan Secure Health Insurance Policy) to answer specific, high-stakes user queries with 100% accuracy."
+    "You must cite the section and clause number for every answer you write related to the Titan Secure Health Insurance Policy."
+    "Keep the answer very simple and easy to understand.\n"
+    "Question: {question} \n"
+    "Context: {context}"
+)
+
+def generate_answer(state: MessagesState):
+    """Generate an answer"""
+    question = [msg.content for msg in state["messages"] if isinstance(msg, HumanMessage)][-1]
+    context = state["messages"][-1].content
+    prompt = GENERATE_PROMPT.format(question=question, context=context)
+    response = grader_model.invoke([{"role" : "user", "content" : prompt}])
+    return {"messages" : [response]}
+
+workflow = StateGraph(MessagesState)
+workflow.add_node(generate_query_or_respond)
+workflow.add_node("retrieve", ToolNode(tools))
+workflow.add_node(rewrite_question)
+workflow.add_node(generate_answer)
+
+workflow.add_edge(START, "generate_query_or_respond")
+workflow.add_conditional_edges(
+    "generate_query_or_respond",
+    tools_condition,
+    {
+        "tools" : "retrieve",
+        END: END,
+    }
+)
+workflow.add_conditional_edges(
+    "retrieve",
+    grade_documents,
+)
+workflow.add_edge("generate_answer", END)
+workflow.add_edge("rewrite_question", "generate_query_or_respond")
+graph = workflow.compile()
+graph.get_graph().draw_mermaid_png()
+
+def contact_llm(msg: str):
+    result = []
+    for chunk in graph.stream(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": msg,
+                }
+            ]
+        }
+    ):
+        for node, update in chunk.items():
+            result.append(update["messages"][-1])
+    return result
